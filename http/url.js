77b6702b5f5a54492adaef2b7312cb19979eb3ce
qinggui <874498723@qq.com>
2020-01-16 16:49:18 +0800

    1

--- http/url.js
+++
@@ -1,7 +0,0 @@
-export const baseUrl = 'https://';
-
-// 用户模块
-export const loginURL = baseUrl + 'wechat/user/login'; // 微信登陆
-export const getPhonenumberURL = baseUrl + 'wechat/user/getPhonenumber'; // 用户手机信息
-export const getUserInfoUrl = baseUrl + 'wechat/user/getUserInfo'; // 获取用户信息
-export const updateUserInfoUrl = baseUrl + 'wechat/user/updateUserInfo'; // 更新用户信息
